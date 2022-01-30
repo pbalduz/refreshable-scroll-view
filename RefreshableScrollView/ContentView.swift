3375dc26206e03ff5cea9c91f89e3c658ed6a13c
Pablo Balduz <pbalduz@gmail.com>
2022-01-30 21:53:31 +0100

    Initial Commit

---
+++ RefreshableScrollView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RefreshableScrollView
+//
+//  Created by Pablo Balduz on 30/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
