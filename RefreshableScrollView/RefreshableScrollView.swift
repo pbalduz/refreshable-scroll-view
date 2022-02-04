4cb7cc7e1ad44bfcfc54f844ff9913fd77431a27
Pablo Balduz <pbalduz@gmail.com>
2022-02-04 22:23:16 +0100

    Clean up by setting visibleRefreshOffset as computed property

--- RefreshableScrollView/RefreshableScrollView.swift
+++ RefreshableScrollView/RefreshableScrollView.swift
@@ -1,14 +1,24 @@
 import SwiftUI
 
 struct RefreshableScrollView: View {
-    @State private var scrollViewOffset: CGFloat = .zero
     @State private var isRefreshing: Bool = false
-    @State private var visibleRefreshOffset: CGFloat = .zero
+    @State private var scrollViewOffset: CGFloat = .zero
+    
+    private var refreshContentHeight: CGFloat = 60
+    private var threshold: CGFloat = 100
+    
+    private var visibleRefreshOffset: CGFloat {
+        guard isRefreshing else { return .zero }
+        return max(
+            0,
+            refreshContentHeight - refreshContentHeight * scrollViewOffset / (refreshContentHeight + 30)
+        )
+    }
     
     var body: some View {
         ZStack(alignment: .top) {
             Color.orange
-                .frame(height: 60)
+                .frame(height: refreshContentHeight)
             GeometryReader { staticViewProxy in
                 Color.clear
                     .preference(
@@ -25,7 +35,6 @@ struct RefreshableScrollView: View {
             ScrollView {
                 VStack(spacing: 0) {
                     Color.clear
-                        .opacity(0.5)
                         .frame(height: visibleRefreshOffset)
                     VStack {
                         ForEach(0...4, id: \.self) { _ in
@@ -58,18 +67,12 @@ struct RefreshableScrollView: View {
             }
             scrollViewOffset = last.offset - first.offset
             
-            if isRefreshing {
-                if scrollViewOffset <= 90 {
-                    visibleRefreshOffset = 60 - 60 * scrollViewOffset / 90
-                }
-            }
-            
-            if scrollViewOffset > 100 {
+            if scrollViewOffset > threshold {
+                guard !isRefreshing else { return }
                 isRefreshing = true
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                     withAnimation {
                         self.isRefreshing = false
-                        self.visibleRefreshOffset = .zero
                     }
                 }
             }
