5aeff346a41a039c24a1137a7a049675145cde25
Pablo Balduz <9513953+pbalduz@users.noreply.github.com>
2022-02-26 20:12:32 +0100

    Create demo project in repo

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Pablo Balduz on 16/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Pablo Balduz on 16/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
